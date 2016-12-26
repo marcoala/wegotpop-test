5f4acb982de8c5cfcfdd1c47d98faccd3ce151b4
Marco Alabruzzo <marco.alabruzzo@growthstreet.co.uk>
2016-12-26 21:00:48 +0000

    Intial structure of a flask application

---
+++ app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf8 -*-
+import os
+import datetime
+from envparse import Env
+
+from flask import Flask, json
+from flask.ext.sqlalchemy import SQLAlchemy
+
+env = Env()
+env.read_envfile()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+@app.route('/')
+def home():
+    data = {
+        'message': 'pong',
+        'time': datetime.datetime.now()
+    }
+    return json.jsonify(data)
+
+
+@app.route('/robots.txt')
+def robots():
+    res = app.make_response('User-agent: *\nAllow: /')
+    res.mimetype = 'text/plain'
+    return res
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
